0afce4d9ab9197680795e8d02eb0ce7a67c7cb24
burakonurerten <burakonurerten@gmail.com>
2024-04-12 09:07:55 +0300

    add functionality to sidebar

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,10 @@
 import { SideBar } from "./SideBar";
+import { Treatments } from "./Treatments";
 
 export default function Home() {
     return (
       <div style={{margin:"0"}}>
         <SideBar ></SideBar>
-
       </div>
     );
   }
--- src/SideBar.js
+++ src/SideBar.js
@@ -2,12 +2,55 @@ import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import React, { useState } from "react";
 import "./SideBar.css";
 import HealingIcon from "@mui/icons-material/Healing";
-import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Treatments } from "./Treatments";
 
 export const SideBar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [showTreatment, setShowTreatment] = useState(false);
+  const [showDepartment, setShowDepartment] = useState(false);
+  const [showHospital, setShowHospital] = useState(false);
+  const [showDoctor, setShowDoctor] = useState(false);
+  const [showPatient, setShowPatient] = useState(false);
+
+  const handleMenuItemClick = (itemName) => {
+    if(itemName === "Treatments"){
+      setShowTreatment(true)
+      setShowDepartment(false)
+      setShowHospital(false)
+      setShowDoctor(false)
+      setShowPatient(false)
+    }
+
+    if(itemName === "Deparments"){
+      setShowTreatment(false)
+      setShowDepartment(true)
+      setShowHospital(false)  
+      setShowDoctor(false)
+      setShowPatient(false)
+    }
+    if(itemName === "Hospitals"){
+      setShowTreatment(false)
+      setShowDepartment(false)
+      setShowHospital(true)
+      setShowDoctor(false)
+      setShowPatient(false)
+    }
+    if(itemName === "Doctors"){
+      setShowTreatment(false)
+      setShowDepartment(false)
+      setShowHospital(false)
+      setShowDoctor(true)
+      setShowPatient(false)
+    }if(itemName === "Patients"){
+      setShowTreatment(false)
+      setShowDepartment(false)
+      setShowHospital(false)
+      setShowDoctor(false)
+      setShowPatient(true)
+    }
+  }
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -19,10 +62,11 @@ export const SideBar = () => {
 
   return (
     <>
-      
       {sidebarOpen && (
-        <Sidebar  backgroundColor="#3c879e" style={{ position: "fixed", height:"100%"}}>
-          
+        <Sidebar
+          backgroundColor="#3c879e"
+          style={{ position: "fixed", height: "100%" }}
+        >
           <div
             style={{
               display: "flex",
@@ -30,26 +74,69 @@ export const SideBar = () => {
               marginRight: "190px",
             }}
           >
-              <HealingIcon aria-label="logo" edge="start" style={{ color: "#fff", paddingLeft:"20px", paddingRight:"10px" }} />
+            <HealingIcon
+              aria-label="logo"
+              edge="start"
+              style={{
+                color: "#fff",
+                paddingLeft: "20px",
+                paddingRight: "10px",
+              }}
+            />
 
             <h2 style={{ color: "#fff", fontWeight: "bold" }}>HealTrip</h2>
-              <CloseIcon style={{color:"#fff", cursor: "pointer", marginLeft:"50px"}} onClick={toggleSidebar} />
+            <CloseIcon
+              style={{ color: "#fff", cursor: "pointer", marginLeft: "50px" }}
+              onClick={toggleSidebar}
+            />
           </div>
 
           <Menu>
-            <MenuItem className="custom-menu-item">Treatments</MenuItem>
-            <MenuItem className="custom-menu-item">Departments</MenuItem>
-            <MenuItem className="custom-menu-item">Hospitals</MenuItem>
-            <MenuItem className="custom-menu-item">Doctors</MenuItem>
-            <MenuItem className="custom-menu-item">Patients</MenuItem>
+            <MenuItem
+              className="custom-menu-item"
+              onClick={() => handleMenuItemClick("Treatments")}
+            >
+              Treatments
+            </MenuItem>
+            <MenuItem
+              className="custom-menu-item"
+              onClick={() => handleMenuItemClick("Departments")}
+            >
+              Departments
+            </MenuItem>
+            <MenuItem
+              className="custom-menu-item"
+              onClick={() => handleMenuItemClick("Hospitals")}
+            >
+              Hospitals
+            </MenuItem>
+            <MenuItem
+              className="custom-menu-item"
+              onClick={() => handleMenuItemClick("Doctors")}
+            >
+              Doctors
+            </MenuItem>
+            <MenuItem
+              className="custom-menu-item"
+              onClick={() => handleMenuItemClick("Patients")}
+            >
+              Patients
+            </MenuItem>
           </Menu>
         </Sidebar>
       )}
-      
-      <div style={{ display: "flex", alignItems: "center", height: "100vh"}}>
-        <div>
-        <ArrowForwardIosIcon id="arrow-icon" onClick={toggleSidebar} style={{ cursor: "pointer", color:"#295d6d" }} />
 
+      <div>
+        <div>
+          <ArrowForwardIosIcon
+            onClick={toggleSidebar}
+            style={{
+              cursor: "pointer",
+              width:"30px",
+              height:"30px",
+              color: "#3c879e"
+            }}
+          />
         </div>
       </div>
     </>
